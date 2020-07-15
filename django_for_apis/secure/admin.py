9a028b72c7a5024541ef92bc6d07a60d3ccc8c6d
manjunathjidagi <manjunathjidagi>
2020-07-15 20:20:16 +0530

    * Added New Table to store extra information of Users

--- django_for_apis/secure/admin.py
+++ django_for_apis/secure/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 
 # Register your models here.
-# from .models import User
-# admin.site.register(User)
+from .models import Profile
+admin.site.register(Profile)
