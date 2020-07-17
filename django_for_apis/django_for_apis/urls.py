99e7e18d14073a83850df64aa68930fbd7747f7c
manjunathjidagi <manjunathjidagi>
2020-07-17 10:39:23 +0530

    * Now the Project Serves Static Files

--- django_for_apis/django_for_apis/urls.py
+++ django_for_apis/django_for_apis/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('secure/', include('secure.urls')),
+    path('api/secure/', include('secure.urls')),
     path('admin/', admin.site.urls),
 ]
